241d7c81d6c0cda9cba6e917f627fe2c121a3461
Nick <thedeadofblackfire@yahoo.com>
2017-12-18 20:10:20 +0100

    Mon Dec 18 19:10:20 GMT 2017

---
+++ www/js/app/app.push.js
@@ -0,0 +1,138 @@
+
+// ---------------------
+// PUSH notifications
+// ---------------------
+app.push = { 
+	
+	getHWId: function() {		
+		return device.uuid || ''; // none for browser
+	},
+	
+	init: function() {
+		console.log('PUSH - init');
+		
+		var push = PushNotification.init({
+			android: {
+			},
+			browser: {
+				pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+			},
+			ios: {
+				alert: "true",
+				badge: true,
+				sound: 'true'
+			},
+			windows: {}
+		});
+		
+		push.on('error', function(e) {
+		  console.log("PUSH - error = " + e.message);
+		});
+		
+		push.on('registration', function(data) {		
+			console.log('PUSH - registrationId: '+data.registrationId);
+			console.log('PUSH - registrationType: '+data.registrationType);
+				
+			var oldRegId = localStorage.getItem('registrationId');
+			if (oldRegId !== data.registrationId) {
+						
+				// Post registrationId to your app server as the value has changed										
+				app.push.register(data.registrationId, function(d) { 
+						console.log("PUSH - api register success: " + JSON.stringify(d));									                                									
+					
+						// Save new registration ID
+						localStorage.setItem('registrationId', data.registrationId);
+					}, 
+					function(xhr) {
+						console.log("PUSH - api register fail" +  xhr);
+					}
+				);		
+				
+			}			
+			
+		});
+
+		push.on('notification', function(data) {
+			console.log('PUSH - notification event');
+			console.log( JSON.stringify(data) );
+			console.log(data.message);
+			console.log(data.title);
+			console.log(data.count);
+			console.log(data.sound);
+			console.log(data.image);
+			console.log(data.additionalData);
+					
+			//Notification was received on device tray and tapped by the user.
+			fw7.modal({
+				title: data.title || 'Notification',
+				text: '<small>'+data.message+'</small>',
+				buttons: [
+				  {
+					text: 'Fermer',
+					bold: true
+				  },
+				]
+			});
+		});
+	},
+	
+	register : function(token, lambda, lambdaerror) {
+		console.log('PUSH register');			               
+		
+		var offset = new Date().getTimezoneOffset() * 60;        //in seconds
+						
+		var language = window.navigator.language;
+		var lang = 'en';
+		if (language) {
+			lang = language.substring(0,2); 
+		}				
+			
+		var devicePlatform = device.platform || '';
+		var deviceModel = device.model || '';
+		var deviceVersion = device.version || '';
+
+		var params = {	
+			user_id : objUser.user_id,
+			app_code : app_settings && app_settings.push_app || "app",
+			app_version : app_settings && app_settings.version || "x",
+			push_token : token,
+			language : lang,
+			hwid : app.push.getHWId(),
+			timezone : offset,
+			device_platform : devicePlatform,
+			device_model : deviceModel,
+			device_version : deviceVersion
+		};    
+		//console.log(params);				
+						
+		app.auth.remoteAjax({
+				url: app_settings.api_url+"/notification/register", 
+				method: 'POST',
+				jwt: false,
+				data: params,
+				cbWin: lambda,
+				cbFail: lambdaerror
+		});	
+	
+	},
+	
+	unregister : function(lambda, lambdaerror) {
+		console.log('PUSH unregister');			          
+		
+		var params = {                                                                                    
+			app_code : app_settings && app_settings.push_app || "app",
+			hwid : app.push.getHWId()                             
+		};
+
+		app.auth.remoteAjax({
+				url: app_settings.api_url+"/notification/unregister", 
+				method: 'POST',
+				jwt: false,
+				data: params,
+				cbWin: lambda,
+				cbFail: lambdaerror
+		});	
+			
+	}          
+	
+};
---
+++ www/js/settings.js
@@ -0,0 +1,18 @@
+var app_settings = {  
+	'platform': 'Android',
+	'platform_both': false,
+	'force_firebase_robo': false,
+	'package_name': 'Livechat',
+	'package_id': 'com.blastis.livechat',
+	'push_app': 'livechat',
+    'push_senderID': '753736220538',
+	'api_url': 'https://api.localhands.com/v1',
+	'api_url_staging': 'https://staging-api.localhands.com/v1',	
+	'api_url_remote': 'https://localhands-api.petimelo.com/v1',	
+	'api_url_dev': 'http://api.localhands.local/v1',	
+	'messaging_url': 'https://push.localhands.com',
+	'env': 'production',
+	'version': '1.1.0',
+    'build': "325",
+    'release_time': '2017.12.07 11:00'
+};
